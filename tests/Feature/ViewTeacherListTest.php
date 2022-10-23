ab31aef2eb0ac38ce5cd9c41d398af59944f5cc4
GlennKimbleJr <glennkimble@yahoo.com>
2022-10-23 17:34:19 -0400

    Fix main.index route.

---
+++ tests/Feature/ViewTeacherListTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ViewTeacherListTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function the_page_loads()
+    {
+        $this->get(route('main.index'))->assertStatus(200);
+    }
+}
